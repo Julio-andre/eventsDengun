dba6037957a3009ee0703743d0b75cd12b37e44d
Julio-andre <julio-andre@dengun.com>
2018-11-06 15:41:38 +0000

    Full Project Repaird: dont lock at the logo

---
+++ App.js
@@ -0,0 +1,78 @@
+import { Navigation } from 'react-native-navigation';
+import ConfigureStore from './src/components/Store/config';
+import { Provider } from 'react-redux';
+
+import Login from './src/components/views/Login';
+import Home from './src/components/views/Home';
+import AddPost from './src/components/views/Admin/AddPost';
+import NotAllow from './src/components/views/Admin/AddPost/notallow';
+import SidedrawerComponenent from './src/components/views/Sidedrawer';
+import UserPosts from './src/components/views/Admin/UserPosts';
+import Article from './src/components/views/Article';
+
+
+const store = ConfigureStore();
+
+Navigation.registerComponent(
+  "eventsDengun.Login",
+  ()=>
+  Login,
+  store,
+  Provider
+);
+
+Navigation.registerComponent(
+  "eventsDengun.Home",
+  ()=>
+  Home,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "eventsDengun.AddPost",
+  ()=>
+  AddPost,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "eventsDengun.SidedrawerComponenent",
+  ()=>
+  SidedrawerComponenent,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "eventsDengun.UserPosts",
+  ()=>
+  UserPosts,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "eventsDengun.Article",
+  ()=>
+  Article,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "eventsDengun.NotAllow",
+  ()=>
+  NotAllow,
+  store,
+  Provider
+);
+
+
+
+
+export default () => Navigation.startSingleScreenApp({
+  screen:{
+    screen:"eventsDengun.Login",
+    title:"Login",
+    navigatorStyle:{
+      navBarHidden:true
+    }
+  }
+})
---
+++ src/components/views/Login/logo.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Animated, Easing } from 'react-native';
+
+
+class Logo extends Component {
+
+    state = {
+        denAnim: new Animated.Value(0),
+        gunAnim: new Animated.Value(0),
+        evenAnim: new Animated.Value(0),
+    }
+
+    componentWillMount(){
+        Animated.sequence([
+            Animated.timing(this.state.denAnim,{
+                toValue:1,
+                duration:1000,
+                easing:Easing.easeOutCubic
+            }),
+            Animated.timing(this.state.gunAnim,{
+                toValue:1,
+                duration:500,
+                easing:Easing.easeOutCubic
+            }),
+            Animated.timing(this.state.evenAnim,{
+                toValue:1,
+                duration:1000,
+                easing:Easing.easeOutCubic
+            })
+        ]).start(()=>{
+            this.props.showLogin()
+        })
+    }
+
+
+    render(){
+        return(
+           <View>
+               <View style={
+                    this.props.orientation === "portrait" 
+                    ? styles.logoStylesPortrait
+                    : styles.logoStylesLandscape
+               }>
+                    <Animated.View style={{
+                        opacity: this.state.denAnim,
+                        top: this.state.denAnim.interpolate({
+                            inputRange:[0,1],
+                            outputRange:[100,0]
+                        })
+                    }}
+                    >
+                        <Text style={styles.den}>DEN</Text>
+                    </Animated.View>
+                    <Animated.View style={{
+                        opacity:this.state.gunAnim
+                    }}
+                    >
+                        <Text style={styles.gun}>GUN</Text>
+                    </Animated.View>
+                    <Animated.View style={{
+                        opacity:this.state.evenAnim
+                    }}
+                    >
+                        <Text style={styles.even}>Events</Text>
+                    </Animated.View>
+
+               </View>
+           </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({ 
+    logoStylesPortrait:{
+        marginTop:50,
+        flex:1,
+        flexDirection: 'row',
+        maxHeight:100
+    },
+    logoStylesLandscape:{
+        marginTop:20,
+        flex:1,
+        flexDirection: 'row',
+        maxHeight:50
+    },
+    den:{
+        fontSize: 40,
+        fontFamily: 'RobotoCondensed-Regular',
+        color:'#555555'
+    },
+    gun:{
+        fontSize: 40,
+        fontFamily: 'RobotoCondensed-Regular',
+        color:'#00ADA9'
+    },
+    even:{
+        fontSize: 40,
+        fontFamily: 'RobotoCondensed-Regular',
+        color:'#555555'
+    },
+})
+
+export default Logo;
